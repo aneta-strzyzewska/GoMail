1867389239fd464e26d093fccc93cae02e201e65
Aneta <aneta.strzyzewska@outlook.com>
2023-06-21 15:48:18 +0200

    Implement a simple template page with a form

--- src/main.go
+++ src/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type PageData struct {
+	PageTitle string
+	Message   EmailMessage
+}
+
+type EmailMessage struct {
+	Address string
+	Subject string
+	Message string
+}
+
 func main() {
-	http.HandleFunc("/try-go", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world! %s", r.URL.Path)
+	form := template.Must(template.ParseFiles("res/form.html"))
+	http.HandleFunc("/go-mail", func(w http.ResponseWriter, r *http.Request) {
+		message := EmailMessage{
+			Address: r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		data := PageData{
+			PageTitle: "GoMail",
+			Message:   message,
+		}
+
+		form.Execute(w, data)
 	})
 
 	http.ListenAndServe(":80", nil)
