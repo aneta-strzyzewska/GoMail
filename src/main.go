e6da7ebe58bc4ba0fc49cd9fa74e9ef1e8284d59
Aneta <aneta.strzyzewska@outlook.com>
2023-06-24 10:44:57 +0200

    Implement Mailgun fallback

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"html/template"
 	"log"
@@ -46,11 +47,12 @@ func main() {
 }
 
 func sendMessage(message EmailMessage) {
-	_, err := handleSendGrid(message)
-	if err != nil {
-		log.Printf("Could not send email, error: %s", err.Error())
+	resp, err := handleSendGrid(message)
+	if err != nil || resp.StatusCode != 200 {
+		log.Printf("Could not send email")
+		log.Printf("Attempting to send via fallback")
+		handleMailgun(message)
 	}
-
 }
 
 func handleSendGrid(message EmailMessage) (*http.Response, error) {
@@ -69,4 +71,27 @@ func handleSendGrid(message EmailMessage) (*http.Response, error) {
 	req.Header.Add("Accept", "*/*")
 
 	return client.Do(req)
+}
+
+func handleMailgun(message EmailMessage) (*http.Response, error) {
+	client := &http.Client{}
+
+	to := message.Address
+	subject := url.QueryEscape(message.Subject)
+	text := url.QueryEscape(message.Text)
+	from := url.QueryEscape(fmt.Sprintf("Go Mail <%s>", "mailgun@sandbox1f13a792e65646d7ac6126cbcf158f5f.mailgun.org"))
+
+	query := fmt.Sprintf("https://api.mailgun.net/v3/sandbox1f13a792e65646d7ac6126cbcf158f5f.mailgun.org/messages?to=%s&subject=%s&text=%s&from=%s", to, subject, text, from)
+	apiKey := os.Getenv("MAILGUN_API_KEY")
+
+	req, _ := http.NewRequest("POST", query, nil)
+	req.Header.Add("Authorization", "Basic "+basicAuth("api", apiKey))
+	req.Header.Add("Accept", "*/*")
+
+	return client.Do(req)
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
 }
